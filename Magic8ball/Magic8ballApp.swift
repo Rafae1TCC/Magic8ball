24cfa093e501dbc3e3f757eb0c72ff1d8dabc7d2
Rafael Cabrera <rafael.cabrera32@uabc.edu.mx>
2026-09-03 11:43:44 -0700

    Initial Commit

---
+++ Magic8ball/Magic8ballApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8ballApp.swift
+//  Magic8ball
+//
+//  Created by Rafael Cabrera on 9/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Magic8ballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
